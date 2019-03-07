152a38140299980e9b30e7480ad33848b2810c0d
pdswift <38026357+pdswift@users.noreply.github.com>
2019-03-07 08:21:54 -0800

    Add files via upload

--- rest_web.py
+++ rest_web.py
@@ -1,28 +1,43 @@
 #This script starts a web server on port 5000, searches and updates the zip cod database
 #CNA 335, winter 19
 #Parker Swift, pdsswift@student.rtc.edu
+#some of ths code was Wyatt
 
-
-
+import mysql.connector
 from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
-@app.route('/search/<name>')
+conn = mysql.connector.connect(user='root', password='',
+                                  host='127.0.0.1',
+                                  database=' zip code data',
+                               buffered = True)
+cursor = conn.cursor()
+
+print('connected to  zip code data database')
+
+
+
+@app.route('/success/<name>')
 def success(name):
-   return 'welcome %s' % name
+    cursor.execute("SELECT * FROM `zip` WHERE 'zipcode'=%s", [name])
+    name = cursor.fetchall()
+    return '%s' % name
 
-@app.route('/update/')
+@app.route('/nowupdate/<name> <name2>')
+def nowupdate(name, name2):
+    cursor.execute("UPDATE `zip` SET EstimatedPopulation=%s WHERE EstimatedPopulation=%s", (name2, name))
+    return 'success'
+
+@app.route('/search',methods = ['get'])
+def search():
+   user = request.args.get('nm')
+   return redirect(url_for('success',name = user))
+
+@app.route('/update',methods = ['POST', 'GET'])
 def update():
-   return 'test'
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+    user = request.form['zip']
+    user2 = request.form['EstimatedPopulation']
+    return redirect(url_for('nowupdate', name=user, name2=user2))
 
 @app.route('/')
 def root():
